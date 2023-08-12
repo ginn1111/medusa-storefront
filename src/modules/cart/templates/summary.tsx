e3c038858d9790695e1beb4c6549ff9a42caee2b
gin <vanthuanjw@gmail.com>
2023-08-12 17:36:53 +0700

    feat: disable checkout if do not any shipping option

--- src/modules/cart/templates/summary.tsx
+++ src/modules/cart/templates/summary.tsx
@@ -1,6 +1,7 @@
 import { Cart } from "@medusajs/medusa"
 import Button from "@modules/common/components/button"
 import CartTotals from "@modules/common/components/cart-totals"
+import { useShippingOptions } from "medusa-react"
 import Link from "next/link"
 
 type SummaryProps = {
@@ -8,11 +9,17 @@ type SummaryProps = {
 }
 
 const Summary = ({ cart }: SummaryProps) => {
+
+  const { shipping_options, isLoading } = useShippingOptions({ region_id: cart?.region_id })
+
+  const isDisabled = !shipping_options?.length
+  const buttonTitle = isDisabled ? 'Do not have any shipping options for your region :(' : 'Go to checkout'
+
   return (
     <div className="grid grid-cols-1 gap-y-6">
       <CartTotals cart={cart} />
       <Link href="/checkout">
-        <Button>Go to checkout</Button>
+        <Button disabled={isDisabled} isLoading={isLoading}>{buttonTitle}</Button>
       </Link>
     </div>
   )
